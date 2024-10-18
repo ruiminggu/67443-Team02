acc04b41dafe3d1e0c927878d12595a7f508c2e7
ruiminggu <91765134+ruiminggu@users.noreply.github.com>
2024-10-17 22:48:11 -0400

    Initial Commit

---
+++ Team02/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Team02
+//
+//  Created by 顾芮名 on 10/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Team02/Team02App.swift
@@ -0,0 +1,17 @@
+//
+//  Team02App.swift
+//  Team02
+//
+//  Created by 顾芮名 on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Team02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
